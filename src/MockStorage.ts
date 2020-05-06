de53c0dce58301ac4d20f2a23c9c8f2c4bf62aba
Juleo Amosah <teomik129@gmail.com>
2020-05-06 19:10:06 -0400

    Refactor MockStorage

--- src/MockStorage.ts
+++ src/MockStorage.ts
@@ -1,32 +1,29 @@
 import Mem, { ExistsCallback, RemoveCallback } from "abstract-blob-store";
 
+const noop = () => {};
 export class MockStorage {
-  constructor() {
+  private store = new Mem();
+  get metadata() {
+    return this.store;
+  }
+
+  bucket() {
+    const { store } = this;
     return {
-      bucket: () => {
-        // Ugly file mock
-        const store = new Mem();
-        const noop = () => {};
-        return {
-          get metadata() {
-            return store;
-          },
-          file: (key: string) => ({
-            createWriteStream: () => store.createWriteStream({ key }, noop),
-            createReadStream: () => store.createReadStream({ key }),
-            get: (cb1: Function) =>
-              store.exists({ key }, (_, ex) =>
-                ex
-                  ? cb1(null, {
-                      createReadStream: () => store.createReadStream({ key })
-                    })
-                  : cb1("Not found")
-              ),
-            exists: (cb2: ExistsCallback) => store.exists({ key }, cb2),
-            delete: (cb3: RemoveCallback) => store.remove({ key }, cb3)
-          })
-        };
-      }
+      file: (key: string) => ({
+        createWriteStream: () => store.createWriteStream({ key }, noop),
+        createReadStream: () => store.createReadStream({ key }),
+        get: (cb1: Function) =>
+          store.exists({ key }, (_, ex) =>
+            ex
+              ? cb1(null, {
+                  createReadStream: () => store.createReadStream({ key }),
+                })
+              : cb1("Not found")
+          ),
+        exists: (cb2: ExistsCallback) => store.exists({ key }, cb2),
+        delete: (cb3: RemoveCallback) => store.remove({ key }, cb3),
+      }),
     };
   }
 }
--- src/test.ts
+++ src/test.ts
@@ -5,29 +5,29 @@ import { MockStorage } from "./MockStorage";
 
 import { cloudStorage } from "./config.example.json";
 
-rewiremock
-  .module(
+(async () => {
+  const mock = await rewiremock.module(
     () => import("./"),
-    r => ({
+    (r) => ({
       "@google-cloud/storage": r.with({
-        Storage: MockStorage
-      })
+        Storage: MockStorage,
+      }),
     })
-  )
-  .then(mock => {
-    const { gcs } = mock;
-    const { bucket, credentials } = cloudStorage;
+  );
 
-    const common = {
-      setup: function(t, cb) {
-        const store = gcs({ credentials, bucket });
-        cb(null, store);
-      },
-      teardown: function(t, store, blob, cb) {
-        if (blob) store.remove(blob, cb);
-        else cb();
-      }
-    };
+  const { gcs } = mock;
+  const { bucket, credentials } = cloudStorage;
 
-    abstractBlobTests(tape, common);
-  });
+  const common = {
+    setup: function (t, cb) {
+      const store = gcs({ credentials, bucket });
+      cb(null, store);
+    },
+    teardown: function (t, store, blob, cb) {
+      if (blob) store.remove(blob, cb);
+      else cb();
+    },
+  };
+
+  abstractBlobTests(tape, common);
+})();
